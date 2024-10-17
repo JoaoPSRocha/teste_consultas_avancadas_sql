ee606bec7c02c5c3419b4cba0fd6a479d13dd0a3
João Pedro Rocha <156812020+JoaoPSRocha@users.noreply.github.com>
2024-10-17 01:40:39 -0300

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -1,13 +1,13 @@
 USE restaurante;
-/* SELECT nome, categoria FROM produtos WHERE preco > 30 */
-/*SELECT nome, telefone, data_nascimento FROM cliente WHERE YEAR (data_nascimento) < '1985' */
-/* SELECT id_produto, descricao FROM produtos WHERE descricao LIKE "%carne%" */
-/*SELECT nome, categoria FROM produtos
-ORDER BY categoria ASC, nome ASC*/
-/* SELECT * FROM produtos 
+SELECT nome, categoria FROM produtos WHERE preco > 30 
+SELECT nome, telefone, data_nascimento FROM cliente WHERE YEAR (data_nascimento) < '1985' 
+SELECT id_produto, descricao FROM produtos WHERE descricao LIKE "%carne%" 
+SELECT nome, categoria FROM produtos
+ORDER BY categoria ASC, nome ASC
+SELECT * FROM produtos 
 ORDER BY preco DESC 
-LIMIT 5 */
-/* SELECT * FROM produtos WHERE categoria =  'prato principal' 
-LIMIT 2 OFFSET  6 */
+LIMIT 5 /
+SELECT * FROM produtos WHERE categoria =  'prato principal' 
+LIMIT 2 OFFSET  6 
 CREATE TABLE backup_pedidos AS
 SELECT * FROM pedidos
